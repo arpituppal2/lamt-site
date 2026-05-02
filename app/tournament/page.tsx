32094ecfc9d4956acd5a92344b989650b1b1f188
Arpit Uppal <someone040807@gmail.com>
2026-05-01 18:49:34 -0700

    Revise tournament schedule times and events
    Updated the tournament schedule with new event times.

--- app/tournament/page.tsx
+++ app/tournament/page.tsx
@@ -2,13 +2,13 @@ export default function TournamentPage() {
   const schedule = [
     { time: '8:00 AM',  event: 'Contestant Check-In',             location: 'Outside MS 4000A' },
     { time: '8:45 AM',  event: 'Opening Ceremony',                location: 'MS 4000A' },
-    { time: '9:00 AM',  event: 'Secret Team Round',               location: 'MS 4000A, 5200' },
-    { time: '10:45 AM', event: 'Algebra / Number Theory',         location: 'MS 4000A, 5200' },
-    { time: '11:45 AM', event: 'Combinatorics',                   location: 'MS 4000A, 5200' },
-    { time: '12:45 PM', event: 'Lunch & Disputes',                location: 'Court of Sciences' },
-    { time: '1:45 PM',  event: 'Geometry',                        location: 'MS 4000A, 5200' },
-    { time: '3:00 PM',  event: 'Guts Round',                      location: 'MS 4000A, 5200' },
-    { time: '4:30 PM',  event: 'Activities',                      location: 'MS 4000A, 5200' },
+    { time: '9:15 AM',  event: 'Secret Team Round',               location: 'MS 4000A, 5200' },
+    { time: '10:30 AM', event: 'Algebra / Number Theory',         location: 'MS 4000A, 5200' },
+    { time: '11:30 AM', event: 'Combinatorics',                   location: 'MS 4000A, 5200' },
+    { time: '12:30 PM', event: 'Lunch & Disputes',                location: 'Court of Sciences' },
+    { time: '1:30 PM',  event: 'Geometry',                        location: 'MS 4000A, 5200' },
+    { time: '2:45 PM',  event: 'Guts Round',                      location: 'MS 4000A, 5200' },
+    { time: '4:15 PM',  event: 'Activities',                      location: 'MS 4000A, 5200' },
     { time: '6:00 PM',  event: 'Awards Ceremony',                 location: 'MS 4000A' },
   ];
 
